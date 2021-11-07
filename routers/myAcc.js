2105a5a2ecf3230810fdbca429bca3884ad96796
ShikariSohan <moksedur.rahman.sohan@gmail.com>
2021-11-07 18:31:13 +0600

    fixed issue of profile routing

--- routers/myAcc.js
+++ routers/myAcc.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router =  express.Router();
-
-router.get('/:username', (req, res) => {
-    res.render("myAcc");
+const User = require('../models/user')
+router.get('/:id', async (req, res) => {
+    try{
+        const user =  await User.findOne({ _id: req.params.id});
+        console.log(user);
+        res.render("myAcc",{user});
+    }
+    catch(err){
+        console.log(err);
+        res.redirect("/");
+    }
 });
 
 module.exports=router;
