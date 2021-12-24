2fb6a61c15e5ad002bbef18fb8f1569b6260e857
ShikariSohan <moksedur.rahman.sohan@gmail.com>
2021-12-24 20:23:39 +0600

    middleware fixed

--- utilities/middlewares.js
+++ utilities/middlewares.js
@@ -5,14 +5,16 @@ module.exports.isLoggedin = (req,res,next)=>{
     let cookies =
     Object.keys(req.signedCookies).length > 0 ? req.signedCookies : null;
 
-  if (cookies[process.env.cookieUserName]) {
-      token = cookies[process.env.cookieUserName];
+    try{
+      const token = cookies[process.env.cookieUserName];
       const decoded = jwt.verify(token, process.env.jwtString);
       req.user = decoded;
       res.locals.currentUser=req.user;
     }
-  else
+  catch(err)
   {
+    console.log(err)
+    req.flash('error','Please Log in first')
     return res.redirect('/login')
   }
    next();
