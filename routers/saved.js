247697e5b66cf3b97bd80046ab26878cf25d2797
ShikariSohan <moksedur.rahman.sohan@gmail.com>
2022-01-05 12:15:42 +0600

    silly change

--- routers/saved.js
+++ routers/saved.js
@@ -6,10 +6,9 @@ const User = require('../models/user')
 router.get('/:id', async (req, res) => {
     try{
         console.log(typeof req.user._id,typeof req.params.id)
-        console.log(req.user._id,req.params.id)
-        const user_id = new ObjectID(req.params.id); 
-        console.log(user_id,req.params.id)
-        if(user_id.equals(req.user._id)){
+        
+       
+        if(req.user._id === re){
             
         const user =  await User.findOne({ _id: req.params.id});
         res.render("saved",{user,timeAgo});
